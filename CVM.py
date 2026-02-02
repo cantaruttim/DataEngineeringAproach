b1637fe9a33836000a939dd234686f2bc190e767
cantaruttim <cantaruttim@outlook.com>
2026-02-01 23:28:35 -0300

    refactoring

--- CVM.py
+++ CVM.py
@@ -1,8 +1,13 @@
-import requests
-import zipfile
-import io
+from pathlib import Path
 from pyspark.sql import SparkSession
 from utils.utils import *
+
+# CONSTANT VARIABLES
+BASE_FOLDER = Path("cvm_data")
+ENTITY_NAME = "CIA_ABERTA"
+file_name = "dfp_cia_aberta_2025.zip"
+base_url = "https://dados.cvm.gov.br/dados"
+specific_url = "/CIA_ABERTA/DOC/DFP/DADOS/"
 
 spark = (
     SparkSession.builder
@@ -10,59 +15,32 @@ spark = (
         .getOrCreate()
 )
 
-# CONSTANT VARIABLES
-file_name = "dfp_cia_aberta_2025.zip"
-base_url = "https://dados.cvm.gov.br/dados"
-specific_url = "/CIA_ABERTA/DOC/DFP/DADOS/"
+ACRONYMS_CIA = {
+    "dre": "DRE",
+    "bpa": "BPA",
+    "dfc": "DFC",
+    "dmpl": "DMPL",
+    "dfp": "DFP",
+    "inf": "INF"
+}
 
-def read_cvm_cia_data(file_name, base_url, specific_url):
-    url = f"{base_url}{specific_url}{file_name}"
-    extract_path = "./cvm_data"
-
-    print(f"Downloading files: {url} ...")
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        print(f"Status code: {response.status_code} ... ✅ ")
-        with zipfile.ZipFile(io.BytesIO(response.content)) as z:
-            z.extractall(extract_path)
-            csv_filename = z.namelist()[0]
-            print(f"File {csv_filename} extracted successfully!")
-        
-        df = (
-            spark
-                .read
-                .format("csv") 
-                .option("header", "true") 
-                .option("sep", ";") 
-                .option("inferSchema", "true") 
-                .option("encoding", "ISO-8859-1") 
-                .load(f"{extract_path}/{csv_filename}")
-        )
-        
-        print(f"Total rows: {df.count()}")
-
-    else:
-        print(f"Status code: {response.status_code} ... ❌")
-        print("Error while download file ...")
-    
-    return df
-
-df = read_cvm_cia_data(file_name, base_url, specific_url)
-
-df = (
-    df.select(
-        'CNPJ_CIA', 
-        'DT_REFER', 
-        'VERSAO', 
-        'DENOM_CIA', 
-        'CD_CVM', 
-        'CATEG_DOC', 
-        'ID_DOC', 
-        'DT_RECEB'
-    )
+download_and_extract_zip(
+    file_name,
+    base_url,
+    specific_url,
+    BASE_FOLDER
 )
 
-df.show()
+year = extract_year_from_filename(file_name)
+year_dir = move_files_to_year_folder(BASE_FOLDER, year)
 
+year_entity_dir = move_year_to_entity(
+    BASE_FOLDER,
+    ENTITY_NAME,
+    year_dir
+)
 
+organize_by_cia_aberta_acronyms(
+    year_entity_dir,
+    ACRONYMS_CIA
+)
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,98 @@
-
+from pathlib import Path
+import requests
+import zipfile
+import io
 
 # ================================
 # ===== TREATING CNPJ OR CPF =====
 # ================================
+
+
+
+
+# ================================
+# ===== EXTRACTING YEAR FROM =====
+# ================================
+
+def extract_year_from_filename(filename: str) -> str:
+    return filename[-8:-4]
+
+# =====================================
+# ===== DOWNLOAD AND EXTRACT FILE =====
+# =====================================
+
+def download_and_extract_zip(
+    file_name: str,
+    base_url: str,
+    specific_url: str,
+    extract_path: Path
+) -> None:
+
+    url = f"{base_url}{specific_url}{file_name}"
+    print(f"Downloading: {url} ... ")
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+        z.extractall(extract_path)
+
+    print("Extraction completed! ✅ ")
+
+
+# =========================================
+# ===== MOVING FILES TO ENTITY FOLDER =====
+# =========================================
+
+def move_files_to_year_folder(
+    base_folder: Path,
+    year: str
+) -> Path:
+
+    year_dir = base_folder / year
+    year_dir.mkdir(exist_ok=True)
+
+    for item in base_folder.iterdir():
+        if item.is_file() and year in item.name:
+            print(f"Moving {item.name} → {year}/ ✅ ")
+            item.rename(year_dir / item.name)
+
+    return year_dir
+
+def move_year_to_entity(
+    base_folder: Path,
+    entity_name: str,
+    year_dir: Path
+) -> Path:
+
+    entity_dir = base_folder / entity_name
+    entity_dir.mkdir(exist_ok=True)
+
+    target_dir = entity_dir / year_dir.name
+
+    if not target_dir.exists():
+        print(f"Moving {year_dir.name} → {entity_name}/ ✅ ")
+        year_dir.rename(target_dir)
+
+    return target_dir
+
+def organize_by_cia_aberta_acronyms(
+    year_entity_dir: Path,
+    acronyms: dict
+) -> None:
+
+    for file in year_entity_dir.iterdir():
+        if not file.is_file():
+            continue
+
+        filename = file.name.lower()
+
+        for key, folder_name in acronyms.items():
+            if key in filename:
+                target_dir = year_entity_dir / folder_name
+                target_dir.mkdir(exist_ok=True)
+
+                print(f"Moving {file.name} → {folder_name}/ ✅ ")
+                file.rename(target_dir / file.name)
+                break
 
